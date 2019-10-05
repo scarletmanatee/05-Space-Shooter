e51a0a7a6ef4e80a7033fd53be5693d8a988e9ac
ambeer <ambeer@umail.iu.edu>
2019-10-05 16:54:28 -0400

    finished the assignment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys, logging, open_color, arcade
+import sys, logging, os, random, math, open_color, arcade
 
 #check to make sure we are running the right version of Python
 version = (3,7)
@@ -13,6 +13,7 @@ SCREEN_HEIGHT = 600
 MARGIN = 30
 SCREEN_TITLE = "Spooker Shooter"
 
+NUM_ENEMIES = 5
 STARTING_LOCATION = (400,100)
 BULLET_DAMAGE = 10
 ENEMY_HP = 100
@@ -60,10 +61,11 @@ class Window(arcade.Window):
 
         # Call the parent class's init function
         super().__init__(width, height, title)
-
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
         # Make the mouse disappear when it is over the window.
         # So we just see our object, not the pointer.
-        self.set_mouse_visible(False)
+        self.set_mouse_visible(True)
 
         arcade.set_background_color(open_color.orange_9)
         self.bullet_list = arcade.SpriteList()
@@ -74,27 +76,58 @@ class Window(arcade.Window):
 
 
     def setup(self):
-        pass 
+        '''
+        Set up enemies
+        '''
+        for i in range(NUM_ENEMIES):
+            x = 120 * (i+1) + 40
+            y = 500
+            enemy = Enemy((x,y))
+            self.enemy_list.append(enemy)  
 
     def update(self, delta_time):
-        pass
+        self.bullet_list.update()
+        for e in self.enemy_list:
+            damage = arcade.check_for_collision_with_list(e, self.bullet_list)
+            for d in damage:
+                e.hp = e.hp - d.damage
+                d.kill()
+                if e.hp < 0:
+                    e.kill()
+                    self.score = self.score + KILL_SCORE
+                else:
+                    self.score = self.score + HIT_SCORE
+                
+            # check for collision
+            # for every bullet that hits, decrease the hp and then see if it dies
+            # increase the score
+            # e.kill() will remove the enemy sprite from the game
+            # the pass statement is a placeholder. Remove line 81 when you add your code
+            # pass
 
     def on_draw(self):
         """ Called whenever we need to draw the window. """
         arcade.start_render()
-
-
-
+        arcade.draw_text(str(self.score), 20, SCREEN_HEIGHT - 40, open_color.white, 16)
+        self.player.draw()
+        self.bullet_list.draw()
+        self.enemy_list.draw()
 
     def on_mouse_motion(self, x, y, dx, dy):
-        """ Called to update our objects. Happens approximately 60 times per second."""
-        pass
+        '''
+        The player moves left and right with the mouse
+        '''
+        self.player.center_x = x
 
     def on_mouse_press(self, x, y, button, modifiers):
-        """
-        Called when the user presses a mouse button.
-        """
-        pass
+      if button == arcade.MOUSE_BUTTON_LEFT:
+                x = self.player.center_x
+                y = self.player.center_y + 15
+                bullet = Bullet((x,y),(0,10),BULLET_DAMAGE)
+                self.bullet_list.append(bullet)
+            #fire a bullet
+            #the pass statement is a placeholder. Remove line 97 when you add your code
+            # pass
 
     def on_mouse_release(self, x, y, button, modifiers):
         """
@@ -112,14 +145,16 @@ class Window(arcade.Window):
             print("Up")
         elif key == arcade.key.DOWN:
             print("Down")
+        pass
+        
 
     def on_key_release(self, key, modifiers):
         """ Called whenever a user releases a key. """
         pass
 
-
 def main():
     window = Window(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    window.setup()
     arcade.run()
 
 
